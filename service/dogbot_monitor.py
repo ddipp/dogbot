855d91e87e6271229d293a8044a5d3cd8659d7d9
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2022-12-10 16:59:53 +0500

    test GPIO

--- service/dogbot_monitor.py
+++ service/dogbot_monitor.py
@@ -4,10 +4,17 @@ import RPi.GPIO as GPIO
 import time
 
 r1, r2 = 17, 18
+b1 = 23
 
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(b1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
+
+    buttonState = GPIO.input(b1)
+    print('b1 = ' buttonState)
+
     GPIO.setup(r1, GPIO.OUT)
     GPIO.output(r1, GPIO.LOW)
     time.sleep(1)
